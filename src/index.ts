4a7071b78b3a780602fe96c6bb47aff08be8d469
Harper Gao <harper.gao@ringcentral.com>
2022-08-02 15:39:42 +0800

    Move package to module ts entry

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,4 @@
 import request from "./request/request";
-import {
-  isArray,
-  isObject,
-  formatSearchParams,
-  deepClone,
-  quickSort,
-  quickSortStack,
-  flatten,
-  initImgLazyLoad,
-  debounce,
-  throttle,
-} from "./util/index";
-import { EventEmitter } from "./packages/emitter";
-
-export {
-  request,
-  isArray,
-  isObject,
-  formatSearchParams,
-  deepClone,
-  quickSort,
-  quickSortStack,
-  flatten,
-  EventEmitter,
-  initImgLazyLoad,
-  debounce,
-  throttle,
-};
+export { request };
+export * from "./util";
+export * from "./packages";
